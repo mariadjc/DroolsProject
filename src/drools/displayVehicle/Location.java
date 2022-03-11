8311314caec406960663f32c33be88b5c3df7790
Aïcha <a.diez22@usp.ceu.es>
2022-03-11 11:36:09 +0100

    change package name

--- src/drools/displayVehicle/Location.java
+++ src/drools/displayVehicle/Location.java
@@ -1,4 +1,4 @@
-package droolsEvaluation;
+package drools.displayVehicle;
 
 import java.io.Serializable;
 import java.util.Objects;
--- src/drools/displayVehicle/Main.java
+++ src/drools/displayVehicle/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package droolsEvaluation;
+package drools.displayVehicle;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
@@ -20,7 +20,12 @@ public class Main {
         KieSession ksession = kc.newKieSession("vechicleKS");
         
         //Urgency u1 = new Urgency();
-        Location w1 = new Location("Indoors");
+        Location w1 = new Location("Home");
+        Location w2 = new Location("Transit");
+        Location w3 = new Location("Work place");
+        Location w4 = new Location("Mountain");
+        Location w5 = new Location("Beach");
+
         
         //Patient p1 = new Patient(1);
         //Patient p2 = new Patient(2);
@@ -33,14 +38,28 @@ public class Main {
         //Symptom s1 = new Symptom ("chest pressure");
         
         System.out.println(w1);
+        System.out.println(w2);
+        System.out.println(w3);
+        System.out.println(w4);
+        System.out.println(w5);
         
         ksession.insert(w1);
+        ksession.insert(w2);
+        ksession.insert(w3);
+        ksession.insert(w4);
+        ksession.insert(w5);
+        
+        
         
         ksession.fireAllRules();
         
         System.out.println("AFTER");
         
         System.out.println(w1);
+        System.out.println(w2);
+        System.out.println(w3);
+        System.out.println(w4);
+        System.out.println(w5);
         
         ksession.dispose();
         
